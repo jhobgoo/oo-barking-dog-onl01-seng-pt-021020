503afa3050c3974250aef85531ef9a19ce94d5db
Joel Hobgood <jhobgoo@gmail.com>
2020-02-26 02:56:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog
 
   def name=(dogs_name)
-    @this_dogs_name = dogs_name
+    @fido.name = dogs_name
   end
 
   def name(dogs_name)
-    @this_dogs_name
+    @fido.name
   end
 
   def bark

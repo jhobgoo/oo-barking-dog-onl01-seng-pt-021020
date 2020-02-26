3246cceb0424cab9be157148aab99484ddf07183
Joel Hobgood <jhobgoo@gmail.com>
2020-02-26 02:46:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,15 @@
 class Dog
 
-def name=(dog_name)
-  @this_dogs_name = dog_name
-end
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
 
-def name(dog_name)
-  @this_dogs_name
-end
+  def name(dog_name)
+    @this_dogs_name
+  end
 
-def bark
-  puts "Woof!"
-  
+  def bark
+    puts "Woof!"
 
 end
 
